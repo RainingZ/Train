2df27b24c7275af825ee635f80032fdd43c8d276
Alex Zhang <hzalex@gmail.com>
2018-09-25 14:45:45 -0400

    Added global variables for: 	music dispatcher, 	song URL, 	"playing" flag, 	and default volume Minor spelling error fixed Added placeholders for TODOs: 	URL check, 	search by song information, 	majority of music functions Fixed an issue where incorrect parameters are being passed and used in music.js Fixed an issue where youtube api is not imported in music.js Added defaultVolume for play function (testing required)

--- Train.js
+++ Train.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 const fs = require("fs");
 const config = require("./config.json");
 
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
+// This loop reads the /events/ dir and attaches each event file to the appropriate event
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    // Call events with all their proper arguments *after* the 'client' var
     client.on(eventName, (...args) => eventFunction.run(client, ...args));
   });
 });
@@ -21,36 +21,44 @@ client.on("message", message => {
   // Define args
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  let dispatcher;
-  let URL;
-  
+  // Global variables
+  var dispatcher;
+  var URL;
+  var playing = false;
+  var defaultVolume = 0.1;
   // When the command is music related
-  if (command == "play" || command == "pause" || command == "resume" || command == "next" || command == "volumn" || command == "stop") {
+  if (command == "play" || command == "pause" || command == "resume" || command == "next" || command == "volume" || command == "stop") {
     try {
       if (args[0] !== null) {
-        URL = args[0];
+        if (isURL(URL)) {
+          URL = args[0];
+        } else {
+          // TODO: getURL from search results (potentially choices)
+          URL = args[0];
+        }
       }
       let commandFile = require(`./commands/music.js`);
-      commandFile.run(client, message, args, dispatcher, URL);
+      commandFile.run(client, message, command, URL);
     } catch (err) {
       console.error(err);
     }
   }
 
-  // Reads user inputs and look in directory for command js file (CAN BE ATTACKED)
+  // Reads user inputs and look in directory for command js file
   else {
     try {
       let commandFile = require(`./commands/${command}.js`);
       commandFile.run(client, message, args);
     } catch (err) {
-     console.error(err);
+      console.error(err);
     }
   }
 });
 
 client.login(config.token);
 
-// URL check placeholder
-function isURL() {
+// URL check helper func
+function isURL(URL) {
+  // TODO: Check if the input is an URL
   return true;
 }
--- commands/music.js
+++ commands/music.js
@@ -1,26 +1,70 @@
-exports.run = (client, message, args, dispatcher, URL) => {
-    message.channel.send("music").catch(console.error);
+const yt = require('ytdl-core');
 
-    if(message == 'resume'){
+exports.run = (client, message, command, URL) => {
+  // Test Message
+  message.channel.send(command).catch(console.error);
+
+  // When the music is playing
+  if (playing) {
+    // Resume
+    if (command == 'resume') {
       message.channel.send("resumed").catch(console.error);
       dispatcher.resume();
-    }else if(message == 'pause'){
+    }
+
+    // Pause
+    else if (command == 'pause') {
       message.channel.send("paused").catch(console.error);
       dispatcher.pause();
-    }else if(message == 'next'){
-      message.channel.send("next").catch(console.error);
+    }
 
-    }else if(message == 'volume'){
+    // Next
+    else if (command == 'next') {
+      message.channel.send("next").catch(console.error);
+      dispatcher.end();
+      // TODO: Play the next song 
+    }
+
+    // Volume
+    else if (command == 'volume') {
       message.channel.send("volume").catch(console.error);
-    }else if(message == 'stop'){
+      // TODO: Volume takes a parameter between 1-100, applies to the current song only
+      // dispatcher.setVolume: taking 1 to 100, divide by 100 for .volume (probably would not need higher volume than normal)
+      // For dispatcher.volume: 0.5 is half, 1 is normal, 2 is double the normal stream volume
+    }
+
+    // Stop
+    else if (command == 'stop') {
       message.channel.send("stopped").catch(console.error);
       dispatcher.end();
-    }else if(message == 'play'){
+      message.member.voiceChannel.leave();
+    }
+
+    // Play
+    else if (command == 'play') {
+      // TODO: Queue the song
+    }
+  }
+
+  // When the music is NOT playing
+  else {
+    // Play
+    if (command == 'play') {
+      playing = true;
       message.channel.send("music played").catch(console.error);
       message.member.voiceChannel.join()
-			.then(function(connection){
-				dispatcher = msg.guild.voiceConnection.playStream(yt(URL, { audioonly: true }), { passes : 1 });
-				})
-			.catch(console.error);
+        .then(function (connection) {
+          dispatcher = message.guild.voiceConnection.playStream(yt(URL, {
+            audioonly: true
+          }), {
+            passes: 1
+          });
+        })
+        .catch(console.error);
+      dispatcher.setVolume(defaultVolume);
     }
+    else {
+      message.channel.send("No Song is Being Played, Queue a Song :D").catch(console.error);
+    }
+  }
 }
